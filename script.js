70a41cda431cda35f8149f44aa6c7f2ecc55d0d9
mayurdoke6 <mayurdoke@gmail.com>
2025-08-02 18:31:39 -0400

    MayurDoke_Website

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-// Add simple interactivity
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Website Loaded Successfully!");
-});
+// Dynamic Publications Loader
+fetch('publications.json')
+  .then(response => response.json())
+  .then(data => {
+    const list = document.getElementById('pub-list');
+    data.forEach(pub => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <strong>${pub.title}</strong><br>
+        <em>${pub.authors}</em><br>
+        <span>${pub.journal}</span><br>
+        <a href="${pub.link}" target="_blank">Read More</a>
+      `;
+      list.appendChild(li);
+    });
+  })
+  .catch(error => {
+    console.error('Error loading publications:', error);
+  });
 
